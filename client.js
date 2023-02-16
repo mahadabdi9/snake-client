1420418d30c23b65254d52db44ad5fd58ae321b0
mahadabdi9 <mahadabdi9@gmail.com>
2023-02-15 22:23:43 -0500

    updating name

--- client.js
+++ client.js
@@ -1,16 +1,23 @@
 const net = require("net");
+const readline = require("readline");
 const connect = function () {
   const conn = net.createConnection({
     host: "165.227.47.243",
     port: 50541
   });
 
+  conn.on("connect", () => {
+    console.log("")
+  })
+
   // interpret incoming data as text
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
     console.log("connection made successfully");
-  });
+
+    conn.write("Name: MLB");
+    });
 
   return conn;
 };
