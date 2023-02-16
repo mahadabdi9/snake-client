46bf84447645092df0bebc9b37c1f3be524d7fba
mahadabdi9 <mahadabdi9@gmail.com>
2023-02-15 20:52:47 -0500

    refactoring

--- client.js
+++ client.js
@@ -1,9 +1,5 @@
-
-
-// establishes a connection with the game server
-const {connect} = require("./client")
-
-/*function () {
+const net = require("net");
+const connect = function () {
   const conn = net.createConnection({
     host: "165.227.47.243",
     port: 50541
@@ -17,8 +13,6 @@ const {connect} = require("./client")
   });
 
   return conn;
-};*/
+};
 
-console.log("Connecting ...");
-connect();
-
+module.exports = {connect};
