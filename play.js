dc1b39c2046ba977eacac58845982dac315b0b9c
mahadabdi9 <mahadabdi9@gmail.com>
2023-02-17 00:00:03 -0500

    added ctrl c exit function

--- play.js
+++ play.js
@@ -3,6 +3,25 @@
 // establishes a connection with the game server
 const {connect} = require("./client")
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
+
+const handleUserInput = function () {
+  stdin.on('data', () => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+    process.stdout.write('.');
+  });
+};
+
 /*function () {
   const conn = net.createConnection({
     host: "165.227.47.243",
